2b3e90569fb49cd09f3c4e14c4ecf401ef6090b7
joao <joaogabrielcsoares@hotmail.com>
2025-05-13 21:29:17 -0300

    criação do TodoItem

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,3 +1,23 @@
 import { FaTrash } from 'react-icons/fa';
 import styles from './TodoItem.module.css';
 
+const TodoItem = ({ todo, onDelete, onToggle }) => {
+    return(
+        <div className={`${styles.todoItem} ${todo.completed ? styles.completed : ''}`}>
+            <input 
+                type='checkbox'
+                checked={todo.completed}
+                onChange={() => onToggle(todo.id)}
+            />
+            <span>{todo.text}</span>
+            <button
+                onClick={ ()=> onDelete(todo.id)}
+                className={styles.deleteButton}
+            >
+                <FaTrash />
+            </button>
+        </div>
+    )
+}
+
+export default TodoItem
